48d12adeac858ef4ec117cec62b724ee6a0970fb
Tijn <tijncoertjens@gmail.com>
2019-12-31 18:20:06 +0100

    Initial Commit

---
+++ 69/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  69
+//
+//  Created by Tijn Coertjens on 31-12-19.
+//  Copyright © 2019 Tijn Coertjens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
