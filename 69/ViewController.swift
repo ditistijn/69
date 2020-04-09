d1bfc0372326e34ca4339641d3d65ad0fd9da4bd
Tijn Coertjens <tijncoertjens@gmail.com>
2020-04-09 11:41:24 +0200

    69 = nice

--- 69/ViewController.swift
+++ 69/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
+var count = 0
+var nice = "nice"
+
 class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
+    
+   
 
+    @IBAction func niceButton(_ sender: Any) {
+        
+        count = count + 1
+        
+        if count == 69 {
+            
+            label.text = String(count) + ", nice."
+            
+        }   else    {
+            
+            label.text = String(count)
+            
+        }
+        
+        
+    }
 }
 
